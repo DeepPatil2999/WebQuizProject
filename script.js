7ec9b7516c604e4cd90ce5087386ac1dab9632b6
DeepPatil2999 <deep.patil2999@gmail.com>
2025-02-18 18:31:17 +0530

    Added all files

---
+++ script.js
@@ -0,0 +1,165 @@
+// ============== landing page ============== //
+
+document.addEventListener('DOMContentLoaded', function () {
+    const gsbutton = document.querySelector('.gs-button');
+    gsbutton.addEventListener('click', function () {
+        window.location.href = 'language-selection.html';
+    })
+
+});
+
+
+
+// ============== language selection page ============== //
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const langhtml = document.querySelector('.html');
+    langhtml.addEventListener('click', function () {
+        window.location.href = 'level-selection-html.html'
+    })
+
+
+    const langcss = document.querySelector('.css');
+    langcss.addEventListener('click', function () {
+        window.location.href = 'level-selection-css.html'
+    })
+
+    const langjs = document.querySelector('.js');
+    langjs.addEventListener('click', function () {
+        window.location.href = 'level-selection-js.html'
+    })
+
+    const langreact = document.querySelector('.react');
+    langreact.addEventListener('click', function () {
+        window.location.href = 'level-selection-react.html'
+    })
+
+})
+
+
+
+
+// ============== level selection page ============== //
+
+
+// HTML Beginer
+
+document.addEventListener('DOMContentLoaded', function () {
+    const htmlbeginer = document.querySelector('.html-beginer');
+    htmlbeginer.addEventListener('click', function () {
+        window.location.href = 'html-beginer-q.html';
+    })
+})
+
+
+//HTML Intermediate
+
+document.addEventListener('DOMContentLoaded', function () {
+    const htmlintermediate = document.querySelector('.html-intermediate');
+    htmlintermediate.addEventListener('click', function () {
+        window.location.href = 'html-intermediate-q.html';
+    })
+})
+
+
+
+//HTML Advance
+
+document.addEventListener('DOMContentLoaded', function () {
+    const htmladvance = document.querySelector('.html-advance');
+    htmladvance.addEventListener('click', function () {
+        window.location.href = 'html-advance-q.html';
+    })
+})
+
+//CSS Beginer
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cssbeginer = document.querySelector('.css-beginer');
+    cssbeginer.addEventListener('click', function () {
+        window.location.href = 'css-beginer-q.html';
+    })
+})
+
+
+//CSS Intermediate
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cssintermediate = document.querySelector('.css-intermediate');
+    cssintermediate.addEventListener('click', function () {
+        window.location.href = 'css-intermediate-q.html';
+    })
+})
+
+
+//CSS Advance
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cssadvance = document.querySelector('.css-advance');
+    cssadvance.addEventListener('click', function () {
+        window.location.href = 'css-advance-q.html';
+    })
+})
+
+
+
+//JS Beginer
+
+document.addEventListener('DOMContentLoaded', function () {
+    const jsbeginer = document.querySelector('.js-beginer');
+    jsbeginer.addEventListener('click', function () {
+        window.location.href = 'js-beginer-q.html';
+    })
+})
+
+
+//JS Intermediate
+
+document.addEventListener('DOMContentLoaded', function () {
+    const jsintermediate = document.querySelector('.js-intermediate');
+    jsintermediate.addEventListener('click', function () {
+        window.location.href = 'js-intermediate-q.html';
+    })
+})
+
+
+//JS Advance
+
+document.addEventListener('DOMContentLoaded', function () {
+    const jsadvance = document.querySelector('.js-advance');
+    jsadvance.addEventListener('click', function () {
+        window.location.href = 'js-advance-q.html';
+    })
+})
+
+
+
+//React Beginer
+
+document.addEventListener('DOMContentLoaded', function () {
+    const reactbeginer = document.querySelector('.react-beginer');
+    reactbeginer.addEventListener('click', function () {
+        window.location.href = 'react-beginer-q.html';
+    })
+})
+
+
+//React Intermediate
+
+document.addEventListener('DOMContentLoaded', function () {
+    const reactintermediate = document.querySelector('.react-intermediate');
+    reactintermediate.addEventListener('click', function () {
+        window.location.href = 'react-intermediate-q.html';
+    })
+})
+
+
+//React Advance
+
+document.addEventListener('DOMContentLoaded', function () {
+    const reactadvance = document.querySelector('.react-advance');
+    reactadvance.addEventListener('click', function () {
+        window.location.href = 'react-advance-q.html';
+    })
+})
